0c229f7e7cc46bee7946d13bed568f94a27550a2
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2023-01-26 19:32:10 +0530

    Add files via upload

---
+++ Common_Errors.py
@@ -0,0 +1,10 @@
+# print(3/0) # ZeroDivisionError
+# print(hh) # NameError
+# a="hello"
+# print(int(a)) # ValueError
+# b=4.5
+# print(b+"Hello") # TypeError
+# print(['hello'].lower()) # AttributeError
+
+
+# print(dir(__builtins__))
